6572d7333d500b3ad4dc195364c460cf4377e595
Kenton <kec6227@rit.edu>
2012-04-26 18:50:39 -0400

    Basic link discovery

--- ExampleFuzzer/src/com/fuzzer/links/LinkFinder.java
+++ ExampleFuzzer/src/com/fuzzer/links/LinkFinder.java
@@ -15,19 +15,25 @@ public class LinkFinder {
 	WebClient client;
 	
 	public LinkFinder(String page, WebClient client){
+		System.out.println("Finding ALL links on page: " + page);
 		this.client = client;
 		try {
 			currentPage = client.getPage(page);
 		} catch (FailingHttpStatusCodeException e) {
-			e.printStackTrace();
+			System.err.println("Bad status code: " + e.getMessage());
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NullPointerException e){
 			e.printStackTrace();
 		}
 	}
 	
 	public List<HtmlAnchor> findAllLinks(){
+		if(currentPage == null){
+			return null; // This happens if the page returned a 404 in the constructor.
+		}
 		return currentPage.getAnchors();
 	}
 	
--- ExampleFuzzer/src/com/fuzzer/links/SiteReader.java
+++ ExampleFuzzer/src/com/fuzzer/links/SiteReader.java
@@ -3,22 +3,34 @@ package com.fuzzer.links;
 import java.util.HashSet;
 import java.util.List;
 
+import org.apache.commons.logging.LogFactory;
+
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 
 
 public class SiteReader {
 
-	private static final boolean PAGE_DISCOVERY = false;
+	static {
+		// This disables the HTMLUnit apache logging
+		LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+	}
+	
+	private static final boolean PAGE_DISCOVERY = true;
 	private static final boolean PAGE_GUESSING = false;
 	private static final boolean COMPLETE_PARSING = true; //Random page/input when false
 	private static final boolean GUESS_PASSWORDS = false;
 	
-	private static final String SITE = "http://localhost:8080/bodgeit";
+	private static final String SITE = "http://localhost:8080/bodgeit/";
+	
+	// Toggle on when fuzzing the other class' site
+	private static final boolean FUZZING_WEB_CLASS = false;
 	
 	//For the Web Engineering class
 	private static final String SITE_ROOT = "http://vm549-03b.se.rit.edu";
 	private static HashSet<String> totalPages = new HashSet<String>();
+	
+	private static HashSet<String> visitedPages = new HashSet<String>();
 	
 	private static WebClient client;
 	
@@ -31,19 +43,46 @@ public class SiteReader {
 		
 		getLinks(links);
 		
+		if(PAGE_DISCOVERY){
+			printLinks();
+		}
 	}
 	
 	public static void getLinks(List<HtmlAnchor> links){
-		System.out.println("Finding links on site: " + SITE);
+		if(links==null){
+			return;
+		}
 		for(HtmlAnchor link: links){
-			System.out.println(link.getHrefAttribute());
+//			System.out.println(link.getHrefAttribute());
 			if(PAGE_DISCOVERY){
-				totalPages.add(link.getHrefAttribute());
+				totalPages.add(link.getHrefAttribute().split("\\?")[0]);
 			}
+			// Get before the question mark to not count each argument
+			// as a separate link.
+			if(visitedPages.contains(link.getHrefAttribute().split("\\?")[0])){
+//				System.out.println("Already read link : " + link.getHrefAttribute().split("\\?")[0]);
+				continue;
+			}
+			visitedPages.add(link.getHrefAttribute().split("\\?")[0]);
+			LinkFinder finder;
+			if(FUZZING_WEB_CLASS){
+				finder = new LinkFinder(SITE_ROOT + link.getHrefAttribute(), client);
+			}else{
+				finder = new LinkFinder(SITE + "/" + link.getHrefAttribute(), client);
+			}
+			getLinks(finder.findAllLinks());
 		}
 	}
 	
 	public static void runExploitsOnPage(String pageToExploit){
 		
+	}
+	
+	private static void printLinks(){
+		System.out.println("\nAll links:\n\n");
+		for(String s : totalPages){
+			System.out.println(s);
+		}
+		System.out.println("Total pages are : " + totalPages.size());
 	}
 }
